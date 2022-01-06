e4274c0fa7ddf204ca723f35b703b5f9d4cf93f4
unknown <tkdgns25300@gmail.com>
2022-01-06 22:00:13 +0900

    basic server structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+})
