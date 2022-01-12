9e5b17a1133ac82770cadd3dbdb3de4516f62dc6
unknown <tkdgns25300@gmail.com>
2022-01-12 23:03:24 +0900

    complete get all todos route

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const PORT = 3000;
 
 // get all todos
 
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await Pool.query("SELECT * FROM todo");
+
+        res.json(allTodos.rows);
+    } catch(err) {
+        console.error(err.message);
+    }
+})
+
 // get a todo
 
 // create a todo
