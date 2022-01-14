bcb46cf6a6d56fea58867e14b492c2e9deb259f5
unknown <tkdgns25300@gmail.com>
2022-01-14 23:24:38 +0900

    get a todo route

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ app.get("/todos", async (req, res) => {
 
 // get a todo
 
+app.get("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const todo = await Pool.query("SELECT * FROM todo WHERE todo_id - $1", [ id ]);
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 // create a todo
 
 app.post("/todos", async (req, res) => {
