2517e29311c0d65d0b695316c5eeac2e891c526f
unknown <tkdgns25300@gmail.com>
2022-01-17 13:03:22 +0900

    complete post route

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const app = express();
 const pool = require('./db');
-
 const PORT = 3000;
+
+app.use(express.json());
+
+
+// get all todos
+
+
+// get a todo
+
+
+// create a todo
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES ($1) RETURNING * ", 
+            [description]
+        );
+        res.json(newTodo.rows[0]);
+    } catch(err) {
+        console.error(err.message)
+    }
+})
+
+// update a todo
+
+
+// delete a todo
+
+
 
 
 app.listen(PORT, () => {
